098cc117cf6a4c9f0492204439f54e0c29bf1ff9
Kimson Wong <kimsonwong@gmail.com>
2017-01-06 20:09:31 +0800

    dropdown temporary fixed

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,29 +1,31 @@
 $(document).ready(function(){
-  var hChanged = false;
   $("div.search .selected-link").click(function(){
     var menu = $(this).next(),
         search = $(this).parent()
         currentH = $(this).height();
     if(search.height() == $(this).height()) {
       $("div.search-fields").children("div").each(function(){
+        resetSearch();
         $(this).height($(this).height()-5);
         $(this).children(".menu").css("opacity", "0");
         $(this).find(".selected-link").height(70);
-        $(this).height($(this).find(".selected-link").height());      
+        $(this).height($(this).find(".selected-link").height());  
       });
       search.height(menu.height() + (search.height() + 5) + "px");
       menu.css("opacity","1");
       $(this).height(currentH-5);
-      // if(!hChanged)
-        menu.height(menu.height()+5);
-      hChanged = true;
+      menu.height(menu.height()+5);
     }
     else {
       $(this).height(currentH+5);
       menu.css("opacity", "0");
-        menu.height(menu.height()-5);
+      menu.height(menu.height()-5);
       search.height($(this).height() + "px");
     }
   });
-  funtion ()
+  function resetSearch(){
+    $(".category .menu").height(134);
+    $(".date .menu").height(166);
+    $(".type .menu").height(326);
+  }
 });
